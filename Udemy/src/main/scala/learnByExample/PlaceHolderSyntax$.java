2b745d3afa38995fa7d67ea0dc596ed608d2e3e3
Sinoop Joy <joysinoop@gmail.com>
2024-04-09 00:57:43 -0400

    Refactoring

--- Udemy/src/main/scala/learnByExample/PlaceHolderSyntax$.java
+++
@@ -1,8 +0,0 @@
-object PlaceHolderSyntax extends App{
-private def isVIP(firstName:String,lastName:String,IsHighStatus:(String,String)=>Boolean):Boolean={
-        IsHighStatus(firstName,lastName)
-        }
-
-        println(isVIP("Donald","Trump",_=="Donald"&&_=="Trump"))
-        println(isVIP("Not","A Trump",_=="Donald"&&_=="Trump"))
-        }
---
+++ Udemy/src/main/scala/learnByExample/PlaceHolderSyntax.scala
@@ -0,0 +1,10 @@
+package learnByExample
+
+object PlaceHolderSyntax extends App {
+  private def isVIP(firstName: String, lastName: String, IsHighStatus: (String, String) => Boolean): Boolean = {
+    IsHighStatus(firstName, lastName)
+  }
+
+  println(isVIP("Donald", "Trump", _ == "Donald" && _ == "Trump"))
+  println(isVIP("Not", "A Trump", _ == "Donald" && _ == "Trump"))
+}
